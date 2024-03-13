c670fb32d6b7284e05fbbef8d8926aee7ead99d4
Andrei Beltchikov <beltchikov@gmx.de>
2024-03-13 15:01:13 +0000

    wip

--- ext/background.js
+++ ext/background.js
@@ -1,6 +1,38 @@
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      files: ['content.js']
-    });
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  console.log('tab' + tab);
+  return tab;
+}
+
+chrome.action.onClicked.addListener(async (currentTab) => {
+  // TODO iterate tabs  
+  // let queryOptions = { active: true, lastFocusedWindow: true };
+  //   // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  //   let [tab] = await chrome.tabs.query(queryOptions);
+  //  console.log("Tab: " + tab);
+
+  // chrome.tabs.query({currentWindow: true}, function (tabs) { 
+  //   console.log("Tab: " + tabs.length);
+
+  //  });
+
+  var tab = await getCurrentTab();
+  console.log('tab' + tab);
+
+  function poc() { alert('tab')};
+
+  chrome.scripting
+    .executeScript({
+      target: { tabId: currentTab.id },
+      func: poc,
+    })
+    .then(() => console.log("injected a function"));
+
+
+  chrome.scripting.executeScript({
+    target: { tabId: currentTab.id },
+    files: ['content.js']
   });
+});
