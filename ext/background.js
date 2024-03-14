d5955dab68b7bd77f1b656135cbf86f34a87c633
Andrei Beltchikov <beltchikov@gmx.de>
2024-03-14 08:19:48 +0000

    wip

--- ext/background.js
+++ ext/background.js
@@ -1,26 +1,7 @@
-async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
-  console.log('tab' + tab);
-  return tab;
-}
+// service worker
 
 chrome.action.onClicked.addListener(async (currentTab) => {
-  // TODO iterate tabs  
-  // let queryOptions = { active: true, lastFocusedWindow: true };
-  //   // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  //   let [tab] = await chrome.tabs.query(queryOptions);
-  //  console.log("Tab: " + tab);
-
-  // chrome.tabs.query({currentWindow: true}, function (tabs) { 
-  //   console.log("Tab: " + tabs.length);
-
-  //  });
-
-  var tab = await getCurrentTab();
-  console.log('tab' + tab);
-
+  
   function poc() { alert('tab')};
 
   chrome.scripting
