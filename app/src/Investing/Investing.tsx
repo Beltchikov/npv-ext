c38eefe7038968d90862db95b8b441e5d4021eca
Andrei Beltchikov <beltchikov@gmx.de>
2024-03-14 08:54:21 +0000

    adds pos messaging code

--- app/src/Investing/Investing.tsx
+++ app/src/Investing/Investing.tsx
@@ -11,12 +11,12 @@ const Investing = () => {
     useEffect(() => {
         if (shared.localHostOrInvesting()) {
             
-        //     // 1. Send a message to the service worker requesting the user's data
-        // chrome.runtime.sendMessage('get-user-data', (response) => {
-        //     // 3. Got an asynchronous response with the data from the service worker
-        //     console.log('received user data', response);
-        //     //initializeUI(response);
-        // });
+            // 1. Send a message to the service worker requesting the user's data
+        chrome.runtime.sendMessage('get-user-data', (response) => {
+            // 3. Got an asynchronous response with the data from the service worker
+            console.log('received user data', response);
+            //initializeUI(response);
+        });
             
             const modalElement = modalRef.current;
             modalElement.showModal();
