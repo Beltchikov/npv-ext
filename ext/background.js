d34e7df87d6d03c65d26f04c79f7f327c70c69ad
Andrei Beltchikov <beltchikov@gmx.de>
2024-03-15 16:24:45 +0000

    wip

--- ext/background.js
+++ ext/background.js
@@ -1,30 +1,6 @@
 // service worker
 
 chrome.action.onClicked.addListener(async (currentTab) => {
-
-
-  function getCurrentTab(callback) {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    chrome.tabs.query(queryOptions, ([tab]) => {
-      if (chrome.runtime.lastError)
-        console.error("ERR: " + chrome.runtime.lastError);
-      // `tab` will either be a `tabs.Tab` instance or `undefined`.
-      callback(tab);
-    });
-  }
-
-  // function poc() {
-  //   alert('tab');
-  // };
-
-  // chrome.scripting
-  //   .executeScript({
-  //     target: { tabId: currentTab.id },
-  //     func: poc,
-  //   })
-  //   .then(() => console.log("injected a function"));
-
-
   chrome.scripting.executeScript({
     target: { tabId: currentTab.id },
     files: ['content.js']
@@ -34,7 +10,7 @@ chrome.action.onClicked.addListener(async (currentTab) => {
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message === 'get-tabs-info') {
     chrome.tabs.query({ active: false, lastFocusedWindow: true })
-      .then((r) => sendResponse({ ok: r }))
+      .then((r) => sendResponse({ data: r }))
       .catch(e => sendResponse({ err: e.message }));
     return true;
   }
