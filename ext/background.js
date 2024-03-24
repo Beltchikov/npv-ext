1943012d889c8788f567685409264fae54e5819c
Andrei Beltchikov <beltchikov@gmx.de>
2024-03-24 07:14:54 +0000

    cleans code

--- ext/background.js
+++ ext/background.js
@@ -1,33 +1,33 @@
 // service worker
-
 var tabsWaitingForData = [];
 
 // entry point
 chrome.action.onClicked.addListener(async (currentTab) => {
 
-   // Startet React app
+  // start react app
   chrome.scripting.executeScript({
     target: { tabId: currentTab.id },
     files: ['content.bundle.js']
   });
-  
+
+  // query tabs
   chrome.tabs.query({ lastFocusedWindow: true })
     .then(tabs => {
       tabsWaitingForData = [];
       tabs.map((t) => {
         tabsWaitingForData.push({ tabId: t.id, activeTab: t.active, data: undefined })
 
-        if(t.active) chrome.scripting
-        .executeScript({
-          target: { tabId: t.id },
-          files: ['dialog.bundle.js']
-        });
+        // load dialog content script
+        if (t.active) chrome.scripting
+          .executeScript({
+            target: { tabId: t.id },
+            files: ['dialog.bundle.js']
+          });
       });
 
+      // execute parser.js for every tab
       for (var i = 0; i < tabs.length; i++) {
         var tabId = tabs[i].id;
-
-        // Execute parser.js content script
         chrome.scripting
           .executeScript({
             target: { tabId: tabId },
@@ -36,35 +36,31 @@ chrome.action.onClicked.addListener(async (currentTab) => {
       }
     })
     .catch(e => console.log({ err: e.message }));
-
- 
 });
 
 // message listener
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  // TODO remove later
-  if (message === 'get-tabs-info') {
-    chrome.tabs.query({ active: false, lastFocusedWindow: true })
-      .then((r) => sendResponse({ data: r }))
-      .catch(e => sendResponse({ err: e.message }));
-    return true;
-  }
-
-  // message.type === 'dataRow'
+  
   if (message.type === 'dataRow') {
-     tabsWaitingForData = tabsWaitingForData.map((t) => t.tabId === sender.tab.id ? { ...t, ...{ data: message.data } } : t);
-     var noDataObjects = tabsWaitingForData.filter((t) => t.data == undefined);
+    tabsWaitingForData = tabsWaitingForData.map((t) => t.tabId === sender.tab.id ? { ...t, ...{ data: message.data } } : t);
+    var noDataObjects = tabsWaitingForData.filter((t) => t.data == undefined);
 
     if (noDataObjects.length === 0) {
       var activeTabData = tabsWaitingForData.filter((t) => t.activeTab)[0];
       if (console) console.log(`calling dialog on tab id ${activeTabData.tabId}`);
-      
+
       const response = await chrome.tabs.sendMessage(activeTabData.tabId, { type: 'dataRows', data: tabsWaitingForData, sender: 'background' });
-      if(response) if (console) console.log('Response true received')
+      if (response) if (console) console.log('Response true received')
       else if (console) console.log('Response false received')
     }
 
   }
+  else
+  {
+    console.log(`Not implemented for ${message.type}`);
+  }
+
+  sendResponse(true);
 });
 
 // Example Promise.all
