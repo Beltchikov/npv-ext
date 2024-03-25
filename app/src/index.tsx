9e3a28e3e301e4a903fa7d62e76a2f4a1ae3dc18
Andrei Beltchikov <beltchikov@gmx.de>
2024-03-25 11:48:28 +0000

    wip

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.createElement("div");
-rootElement.id = "collector";
-document.body.appendChild(rootElement);
+const idCollector = 'collector';
+if (!document.getElementById(idCollector)) {
+  const rootElement = document.createElement("div");
+  rootElement.id = idCollector;
+  document.body.appendChild(rootElement);
+}
 
 export const collector = ReactDOM.createRoot(
-  document.getElementById('collector') as HTMLElement
+  document.getElementById(idCollector) as HTMLElement
 );
+
 collector.render(
   <React.StrictMode>
     <App />
