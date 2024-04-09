0af43fae6cdd593ed2a2ef7647a9f80c442dbc43
Andrei Beltchikov <beltchikov@gmx.de>
2024-04-09 09:38:04 +0200

    wip

--- app/src/Parsers/TwitterParser.ts
+++ app/src/Parsers/TwitterParser.ts
@@ -11,34 +11,70 @@ export function getDataRow(): Array<string> {
 const getTimeTags = (hoursAgo: number): any => {
     var timeOfEarliestTweet = shared.addHoursToDate(new Date(Date.now()), -1 * hoursAgo)
 
-    var timeElements: Array<HTMLTimeElement> = shared.getElementsByTag('time');
-    console.log('timeElements');
-    console.log(timeElements);
+    // var timeElements: Array<HTMLTimeElement> = shared.getElementsByTag('time');
+    // console.log('timeElements');
+    // console.log(timeElements);
 
-    if (timeElements.length === 0) throw new Error('No time elements found');
-    var lastElement: HTMLTimeElement = timeElements[timeElements.length - 1];
-    console.log('lastElement');
-    console.log(lastElement);
+    // if (timeElements.length === 0) throw new Error('No time elements found');
+    // var lastElement: HTMLTimeElement = timeElements[timeElements.length - 1];
+    // console.log('lastElement');
+    // console.log(lastElement);
 
-    //lastElement.scrollIntoView(false);
-    lastElement.scrollIntoView({ behavior: "smooth", block: "end", inline: "center" });
-    //////////////////
-    var timeElements2: Array<HTMLTimeElement>;
-    setTimeout(() => {
-        timeElements2 = shared.getElementsByTag('time');
+    // lastElement.scrollIntoView({ behavior: "smooth", block: "end", inline: "center" });
+    // //////////////////
+    // var timeElements: Array<HTMLTimeElement>;
+    // setTimeout(() => {
+    //     timeElements = shared.getElementsByTag('time');
 
-        if (timeElements2.length === 0) throw new Error('No time elements found 2');
-        var lastElement2: HTMLTimeElement = timeElements2[timeElements2.length - 1];
-        console.log('lastElement2');
-        console.log(lastElement2);
+    //     if (timeElements.length === 0) throw new Error('No time elements found 2');
+    //     var lastElement2: HTMLTimeElement = timeElements[timeElements.length - 1];
+    //     console.log('lastElement2');
+    //     console.log(lastElement2);
+    // },
+    //     1000);
+
+    ////////////////////////////
+
+    var allTimeElements: Array<HTMLTimeElement> = [];
+    var timeout = 1000;
+    var i = 0;
+    while (i < 2) {
+        if (i == 0) {
+            const timeElements: Array<HTMLTimeElement> = shared.getElementsByTag('time');
+            if (timeElements.length === 0) throw new Error('No time elements found');
+            allTimeElements.concat(timeElements);
+
+            const lastElement: HTMLTimeElement = timeElements[timeElements.length - 1];
+            console.log('lastElement');
+            console.log(lastElement);
+
+            lastElement.scrollIntoView({ behavior: "smooth", block: "end", inline: "center" });
+
+        }
+        else { }
+        setTimeout(() => {
+            const timeElements: Array<HTMLTimeElement> = shared.getElementsByTag('time');
+            if (timeElements.length === 0) throw new Error('No time elements found');
+            allTimeElements.concat(timeElements);
+
+            const lastElement: HTMLTimeElement = timeElements[timeElements.length - 1];
+            console.log('lastElement');
+            console.log(lastElement);
+            
+            lastElement.scrollIntoView({ behavior: "smooth", block: "end", inline: "center" });
         },
-        1000);
+            timeout);
+
+        i++;
+    }
 
 
 
+    // var datesOfTweets = timeElements.map((e: any) => new Date(e.dateTime));
+    // console.log('datesOfTweets');
+    // console.log(datesOfTweets);
 
-
-    var datesOfTweets = timeElements.map((e: any) => new Date(e.dateTime));
+    var datesOfTweets = allTimeElements.map((e: any) => new Date(e.dateTime));
     console.log('datesOfTweets');
     console.log(datesOfTweets);
 
