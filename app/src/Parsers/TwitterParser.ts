c12c59bf3b248db94258188ee1b773b6933d9a4c
Andrei Beltchikov <beltchikov@gmx.de>
2024-04-09 10:50:39 +0200

    wip

--- app/src/Parsers/TwitterParser.ts
+++ app/src/Parsers/TwitterParser.ts
@@ -9,28 +9,31 @@ export function getDataRow(): Array<string> {
 }
 
 const getTimeTags = (hoursAgo: number): any => {
-    var timeOfEarliestTweet = shared.addHoursToDate(new Date(Date.now()), -1 * hoursAgo)
+    var timestampOfEarliestTweet = shared.addHoursToDate(new Date(Date.now()), -1 * hoursAgo)
 
     var allTimeElements: Array<HTMLTimeElement> = [];
     var timeout = 1000;
     var i = 0;
+    var maxTweetCount = 100;
 
     var intervalId = setInterval(() => {
         const timeElements: Array<HTMLTimeElement> = shared.getElementsByTag('time');
         if (timeElements.length === 0) throw new Error('No time elements found');
         allTimeElements = allTimeElements.concat(timeElements);
-        console.log('allTimeElements');
-        console.log(allTimeElements);
-
+        
         const lastElement: HTMLTimeElement = timeElements[timeElements.length - 1];
-        const earliestDate = new Date(lastElement.dateTime);
-        console.log('earliestDate');
-        console.log(earliestDate);
+        const earliestTimestamp = new Date(lastElement.dateTime);
+        console.log('earliestTimestamp');
+        console.log(earliestTimestamp);
 
         lastElement.scrollIntoView({ behavior: "smooth", block: "end", inline: "center" });
         i++;
-        if(i== 10) {
+        if(i== maxTweetCount || earliestTimestamp <= timestampOfEarliestTweet) {
             clearInterval(intervalId);
+
+            if(i===maxTweetCount) console.log(`LIMIT of ${maxTweetCount} reached`);
+            if(earliestTimestamp <= timestampOfEarliestTweet) 
+                console.log(`24 HOURS PERIOD PROCESSED ${earliestTimestamp}`);
 
             var datesOfTweets = allTimeElements.map((e: any) => new Date(e.dateTime));
             console.log('datesOfTweets');
