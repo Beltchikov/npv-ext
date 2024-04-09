b523c67004be976c004e95903e40fb276cdea513
Andrei Beltchikov <beltchikov@gmx.de>
2024-04-09 06:58:43 +0200

    wip

---
+++ app/src/Parsers/TwitterParser.ts
@@ -0,0 +1,11 @@
+import shared from "../shared";
+
+export function getDataRow(): Array<string> {
+    // TODO
+    return ['Switzerland aims to host a high-level ', '@business', '1 Min.'];
+}
+
+export function getHeader(): string {
+    return "Message User Time";
+}
+
