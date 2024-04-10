070777c6d77f53c1061114e9b14b902ff4eb1dcc
Andrei Beltchikov <beltchikov@gmx.de>
2024-04-10 14:44:21 +0200

    wip

--- app/src/Parsers/TwitterParser.ts
+++ app/src/Parsers/TwitterParser.ts
@@ -1,92 +1,54 @@
 import shared from "../shared";
 
 export async function getDataRow(): Promise<Array<Array<string>>> {
-
-    //var timesOfTweets = getTimeTags(24);
-
-    // TODO
-    // return [
-    //     ['Switzerland aims to host a high-level ', '@business', '1 Min.']
-    // ];
     return await getTimeTagsAsync(24);
 }
 
-const getTimeTagsAsync =async  (hoursAgo: number):Promise<Array<Array<string>>>=>{
-    let promise = new Promise<Array<Array<string>>>((resolve, reject) =>{
+const getTimeTagsAsync = async (hoursAgo: number): Promise<Array<Array<string>>> => {
+    let promise = new Promise<Array<Array<string>>>((resolve, reject) => {
         var timestampOfEarliestTweet = shared.addHoursToDate(new Date(Date.now()), -1 * hoursAgo)
 
         var allTimeElements: Array<HTMLTimeElement> = [];
         var timeout = 1000;
         var i = 0;
         var maxTweetCount = 20;
-    
+
         var intervalId = setInterval(() => {
             const timeElements: Array<HTMLTimeElement> = shared.getElementsByTag('time');
             if (timeElements.length === 0) throw new Error('No time elements found');
             allTimeElements = allTimeElements.concat(timeElements);
-    
+
             const lastElement: HTMLTimeElement = timeElements[timeElements.length - 1];
             const earliestTimestamp = new Date(lastElement.dateTime);
             console.log(buildExtractingDataMessage(hoursAgo, i, maxTweetCount, earliestTimestamp));
-    
+
             lastElement.scrollIntoView({ behavior: "smooth", block: "end", inline: "center" });
             i++;
             if (i == maxTweetCount || earliestTimestamp <= timestampOfEarliestTweet) {
                 clearInterval(intervalId);
-    
+
                 if (i === maxTweetCount) console.log(`LIMIT of ${maxTweetCount} reached`);
                 if (earliestTimestamp <= timestampOfEarliestTweet)
                     console.log(`24 HOURS PERIOD PROCESSED ${earliestTimestamp}`);
-    
+
                 var datesOfTweets = allTimeElements.map((e: any) => (new Date(e.dateTime)).toLocaleString('de'));
                 console.log('datesOfTweets');
                 console.log(datesOfTweets);
-    
-                resolve([datesOfTweets]);
+
+                var allData:Array<Array<string>> = datesOfTweets.map<Array<string>>(d=>Array.from([d,"TODO","TODO2"]));
+                
+                // resolve([datesOfTweets,
+                //     Array.from({ length: datesOfTweets.length }, (v: string) => "TODO"),
+                //     Array.from({ length: datesOfTweets.length }, (v: string) => "TODO")
+                // ]);
+
+                resolve(allData);
             }
-    
+
         }, timeout);
     });
 
     return promise;
-}
-
-const getTimeTags = (hoursAgo: number): any => {
-    var timestampOfEarliestTweet = shared.addHoursToDate(new Date(Date.now()), -1 * hoursAgo)
-
-    var allTimeElements: Array<HTMLTimeElement> = [];
-    var timeout = 1000;
-    var i = 0;
-    var maxTweetCount = 1000;
-
-    var intervalId = setInterval(() => {
-        const timeElements: Array<HTMLTimeElement> = shared.getElementsByTag('time');
-        if (timeElements.length === 0) throw new Error('No time elements found');
-        allTimeElements = allTimeElements.concat(timeElements);
-
-        const lastElement: HTMLTimeElement = timeElements[timeElements.length - 1];
-        const earliestTimestamp = new Date(lastElement.dateTime);
-        console.log(buildExtractingDataMessage(hoursAgo, i, maxTweetCount, earliestTimestamp));
-
-        lastElement.scrollIntoView({ behavior: "smooth", block: "end", inline: "center" });
-        i++;
-        if (i == maxTweetCount || earliestTimestamp <= timestampOfEarliestTweet) {
-            clearInterval(intervalId);
-
-            if (i === maxTweetCount) console.log(`LIMIT of ${maxTweetCount} reached`);
-            if (earliestTimestamp <= timestampOfEarliestTweet)
-                console.log(`24 HOURS PERIOD PROCESSED ${earliestTimestamp}`);
-
-            var datesOfTweets = allTimeElements.map((e: any) => (new Date(e.dateTime)).toLocaleString('de'));
-            console.log('datesOfTweets');
-            console.log(datesOfTweets);
-
-            return 'todo';
-        }
-
-    }, timeout);
-
-
 }
 
 const buildExtractingDataMessage = (hoursAgo: number, iterationIdx: number, maxTweetCount: number, earliestTimestamp: Date) => {
