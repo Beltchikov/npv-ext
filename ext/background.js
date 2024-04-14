aaf3a97abe5bb3d5aad0a557b65edee28041e7b6
Andrei Beltchikov <beltchikov@gmx.de>
2024-04-14 07:33:05 +0200

    wip

--- ext/background.js
+++ ext/background.js
@@ -91,7 +91,7 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
 
       //var cummulatedDataArray = tabsRequestedForData.map((t) => t.dataTable.rows);
       let cummulatedDataRows = tabsRequestedForData.map(dt => dt.dataTable);
-      cummulatedDataRows = cummulatedDataRows.reduce((r,n)=>r.rows.concat(n).rows);
+      cummulatedDataRows = cummulatedDataRows.reduce((r, n) => r.rows.concat(n).rows);
       cummulatedDataRows = cummulatedDataRows.rows;
       console.log('cummulatedDataRows');
       console.log(cummulatedDataRows);
@@ -99,33 +99,13 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
       var cummulatedDataArray = [];
       for (var i = 0; i < cummulatedDataRows.length; i++) {
         let row = cummulatedDataRows[i];
-
-        console.log('row');
-        console.log(row);
-
-        // let cells = [];
-        // cells = row.map(r => r.cells);
-
-        // console.log('cells');
-        // console.log(cells);
-
-        console.log(`cummulatedDataArray0`);
-        console.log(cummulatedDataArray);
-
         cummulatedDataArray.push(row.cells);
-        // for (var ii = 0; ii < cells.length; ii++) {
-        //   let cell = cells[ii];
-        //   cummulatedDataArray.push()
-        // }
       }
-
 
       console.log(`cummulatedDataArray`);
       console.log(cummulatedDataArray);
-
-      let shape = getShape(cummulatedDataArray)
       console.log('shape cummulatedDataArray');
-      console.log(shape);
+      console.log(getShape(cummulatedDataArray));
 
       console.log(`Sending message to dialog on tab id ${activeTabData.tabId}`);
       const response = await chrome.tabs.sendMessage(
