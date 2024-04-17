7c0f03be85f60e16f2a524e034bef552fa83f9b1
Andrei Beltchikov <beltchikov@gmx.de>
2024-04-17 09:25:09 +0200

    wip

--- app/src/background.ts
+++ app/src/background.ts
@@ -1,17 +1,6 @@
 import { loadScripts } from './loader';
 import * as shared from "./shared"
-
-class TabDataAndPayload {
-    tabId: number;
-    activeTab: boolean;
-    tabData: string[][]|undefined;
-
-    constructor(tabId:number, activeTab:boolean, tabData:Array<Array<string>>|undefined) {
-      this.tabId = tabId;
-      this.activeTab = activeTab;
-      this.tabData = tabData;
-    }
-  }
+import { TabDataAndPayload } from './tabDataAndPayload';
 
 var tabsRequested:Array<TabDataAndPayload> = [];
 
