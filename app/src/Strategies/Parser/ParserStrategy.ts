06d8a8f76f201249825a2f1d4da95eb2124eadfb
Jogger <beltchikov@gmx.de>
2024-04-19 04:43:09 +0200

    adds onMessageListener function in ParserStrategy

--- app/src/Strategies/Parser/ParserStrategy.ts
+++ app/src/Strategies/Parser/ParserStrategy.ts
@@ -29,4 +29,27 @@ export class ParserStrategy implements IStrategy {
       sendResponse(true);
     });
   }
+
+  async #onMessageListener(message:any, sender:chrome.runtime.MessageSender, sendResponse:(r?:any)=>void)
+  {
+    if (message.target !== 'background') return;
+
+      const messageTypeMap = [
+        { messageType: "strategyEntry", handler: new EntryHandler() },
+        { messageType: "tabData", handler: new TabDataHandler() },
+      ];
+
+      let messageMapEntries = messageTypeMap.filter((m) => m["messageType"] === message.type);
+      if (messageMapEntries.length !== 1)
+        throw new Error(`message map is not proper defined for message type ${message.type}`);
+      let messageEntry = messageMapEntries[0];
+      if (!messageEntry) throw new Error(`Unexpected. messageEntry is undefined.`);
+      let messageHandler = messageEntry.handler;
+
+      this.tabsRequested = await messageHandler.handle(message, sender, this.tabsRequested);
+      console.log('this.tabsRequested');
+      console.log(this.tabsRequested);
+
+      sendResponse(true);
+  }
 }
